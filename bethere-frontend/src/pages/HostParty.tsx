93f8280cda2c3332de1ca56c57c7205bca0e5a61
Yanis Meichtry <yanis@meichtry.dev>
2025-04-04 08:28:28 +0200

    created a Navbar and three Test Pages from the Navbar

---
+++ bethere-frontend/src/pages/HostParty.tsx
@@ -0,0 +1,6 @@
+function HostParty() {
+    return <div>Host Party Page</div>
+  }
+  
+  export default HostParty
+  
---
+++ bethere-frontend/src/pages/Search.tsx
@@ -0,0 +1,6 @@
+function Search() {
+    return <div>Search Page</div>
+  }
+  
+  export default Search
+  
